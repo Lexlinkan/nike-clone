1d19aee513cf59a926de3d2954e3359cd327fa25
Lexlinkan <alexander-lindqvist@live.se>
2023-11-17 21:39:24 +0100

    started nike project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="realtive">
+    Nav
+    <section className="xl:padding-1 padding-b wide:padding-r">
+      Hero
+    </section>
+    <section className="padding">
+      Popular products
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
