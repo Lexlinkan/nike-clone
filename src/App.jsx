4124993452c039e06650632a0c6e82e017b081f5
Lexlinkan <alexander-lindqvist@live.se>
2023-11-18 15:23:08 +0100

    Påbörjat project

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  Nav,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./components";
+
 const App = () => (
   <main className="realtive">
-    Nav
+    <Nav />
     <section className="xl:padding-1 padding-b wide:padding-r">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      Popular products
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="padding-x padding-t pb-8 bg-black">
-      Footer
+      <Footer />
     </section>
   </main>
 );
